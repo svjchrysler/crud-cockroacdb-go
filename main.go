82c064fa2fdc2e4ded2353595f167daa5388d930
Jhon Salguero <svjchrysler@gmail.com>
2017-01-20 17:08:58 -0400

    config gorm with cockroachdb

--- main.go
+++ main.go
@@ -8,7 +8,9 @@ import (
 
 
 type Product struct {
-
+	ID uint `gorm:"primary_key"`
+	Name string `gorm:"type:varchar(100)"`
+	Price float32
 }
 
 
@@ -22,12 +24,28 @@ func conexion() *gorm.DB{
 }
 
 func createTables() {
-	conexion:= conexion()
+	db := conexion()
+
+	if !db.HasTable(&Product{}) {
+		db.CreateTable(&Product{})
+	}
+
+	defer db.Close()
+}
+
+func createProduct() {
+
+	db:= conexion()
+
+	product := Product{Name:"coca cola", Price:10.5}
+
+	db.Create(&product)
+
+	defer db.Close()
 
 }
 
 func main() {
 	createTables()
-
-
+	createProduct()
 }
